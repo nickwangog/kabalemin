7ac564f583bfa74c387c1147bb72b2be0568bfaa
Nicholas Wang <nwang@e1z3r4p20.42.us.org>
2018-05-10 15:58:58 -0700

    a

---
+++ includes/lemin.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lemin.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nwang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/01 23:31:59 by nwang             #+#    #+#             */
+/*   Updated: 2018/05/02 00:21:48 by nwang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEMIN_H
+# define LEMIN_H
+
+# include <unistd.h>
+# include <dirent.h>
+# include <stdlib.h>
+# include <string.h>
+# include <time.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/xattr.h>
+# include <pwd.h>
+# include <grp.h>
+# include <uuid/uuid.h>
+# include <errno.h>
+# include "../minilibx/mlx.h"
+# include "../libft/includes/ft_printf.h"
+# define C_RED		"\x1b[31m"
+# define C_GRN		"\x1b[32m"
+# define C_YEL		"\x1b[33m"
+# define C_BLU		"\x1b[34m"
+# define C_MAG		"\x1b[35m"
+# define C_CYN		"\x1b[36m"
+# define C_WHT		"\x1b[37m"
+# define C_RESET	"\x1b[0m"
+
+typedef struct		s_rooms
+{
+	char			*name;
+	int				num;
+	int				start;
+	int				end;
+}
+
+typedef struct		s_lem
+{
+	int				ant_num;
+}					t_lem;
+
+#endif
--- srcs/read.c
+++ srcs/read.c
@@ -1,23 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   read.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nwang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/01 23:32:22 by nwang             #+#    #+#             */
-/*   Updated: 2018/05/02 00:31:41 by nwang            ###   ########.fr       */
+/*   Created: 2018/05/02 00:31:54 by nwang             #+#    #+#             */
+/*   Updated: 2018/05/02 00:33:41 by nwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		main(int argc, char **argv)
-{	
-	t_lem	*lem;
+void		readmap(t_lem *lem)
+{
 
-	(void)argv;
-	if (argc > 1)
-		ft_printf("usage: ./lem-in < (map)\n");
-	lem = (t_lem *)ft_memalloc(sizeof(t_lem));
-	readmap(lem);
-	return (0);
 }
